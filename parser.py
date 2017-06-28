75d4e68c3a2f037d857e902c3356fdf7047cef7a
Mihandr <mixman100@yandex.ru>
2017-06-29 00:11:41 +0300

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+#headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 YaBrowser/17.6.0.1633 Yowser/2.5 Safari/537.36'}
+URL = "https://vlgg.realty.mail.ru/offer/sale-liv-61027030014258681.html"
+page = requests.get(URL)
+pageContent = page.content
+soup = BeautifulSoup(pageContent, "html.parser")
+price = soup.find("span", {"class": "priceelem"})
+
+
+print(price)
+#print (soup.prettify()) #Вывод дерева
